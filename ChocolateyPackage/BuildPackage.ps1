56cd6ae139682cb8542652c4de4e03d68c879cc2
Bart Joy <bart@ecargo.co.nz>
2018-11-21 15:32:26 +1300

    Set version from Github download URL

--- ChocolateyPackage/BuildPackage.ps1
+++ ChocolateyPackage/BuildPackage.ps1
@@ -3,10 +3,12 @@ $ErrorActionPreference = 'Stop'
 pushd $PSScriptRoot
 try {
     $readme = Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/nzbart/sqlgrep/master/README.md | select -expand Content
-    if($readme -notmatch '\[Download from Github\]\((https://github.com/nzbart/sqlgrep/releases/download/v[^/]+/sqlgrep\.exe)\)') {
+    if($readme -notmatch '\[Download from Github\]\((https://github.com/nzbart/sqlgrep/releases/download/v([^/]+)/sqlgrep\.exe)\)') {
         throw "Couldn't find URL to download latest version from Github readme."
     }
+
     $downloadUrl = $Matches[1]
+    $version = $Matches[2]
 
     if(Test-Path dist) {
         rm -r -fo dist
@@ -15,6 +17,12 @@ try {
     cp -r ../sqlgrep
     cd sqlgrep
     Invoke-WebRequest -UseBasicParsing -Uri $downloadUrl -OutFile "tools\sqlgrep.exe"
+
+    $nuspecPath = resolve-path sqlgrep.nuspec | select -expand providerpath
+    [xml]$nuspec = cat $nuspecPath
+    $nuspec.package.metadata.version = $version
+    $nuspec.Save($nuspecPath)
+
     choco pack
     mv sqlgrep.*.nupkg ..
 }
