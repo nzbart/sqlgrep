e61103cdcacbb05006a6a344b2d566bea5d699fd
Bart Joy <bartjoy@gmail.com>
2018-11-25 05:51:44 +1300

    Only build once
    On Linux, the build configuration needs to be specified via the cmake
    generate step:
    
    -DCMAKE_BUILD_TYPE=MinSizeRel
    
    However, there's an existing bug in SOCI that prevents this from
    working:
    
    https://github.com/SOCI/soci/issues/640
    
    As a workaround, I currently build in Debug mode.

--- ThirdParty/soci/Build.sh
+++ ThirdParty/soci/Build.sh
@@ -3,5 +3,4 @@ rm -rf linuxbuild
 mkdir linuxbuild
 cd linuxbuild
 cmake -DWITH_BOOST=OFF -DSOCI_EMPTY=OFF -DWITH_ORACLE=OFF -DWITH_ODBC=ON -DWITH_MYSQL=OFF -DWITH_POSTGRESQL=OFF -DWITH_FIREBIRD=OFF -DWITH_DB2=OFF -DSOCI_CXX_C11=ON -DSOCI_STATIC=ON -DSOCI_SHARED=OFF -DSOCI_TESTS=OFF ../src
-cmake --build . --config Debug
-cmake --build . --config Release
+cmake --build .
