3a7b06097397280587b85b55e06e460c54913676
Bart Joy <bartjoy@gmail.com>
2018-11-26 20:07:38 +1300

    Replace cout with fmt

--- sqlgrep/sqlgrep.cpp
+++ sqlgrep/sqlgrep.cpp
@@ -186,7 +186,7 @@ auto find_and_display_matches(string_view to_find, int const maximum_results_per
     session sql(parameters);
     sql.set_logger(new database_query_logger);  // `new` is required by SOCI
     auto all_columns = get_all_string_columns(sql);
-    cout << "Searching " << all_columns.size() << " columns for '" << to_find << "'...\n";
+    fmt::print("Searching {} columns for '{}'...\n", all_columns.size(), to_find);
     uint64_t const total_rows = accumulate(begin(all_columns), end(all_columns), 0, [](uint64_t acc, column_details const & b) { return acc + b.number_of_rows; });
     write_verbose("Total number of rows to search: "s + to_string(total_rows) + ".");
     display_all_matches(sql, all_columns, to_find, maximum_results_per_column, total_rows);
@@ -313,7 +313,7 @@ int main(int argc, char** argv)
     try
     {
         find_and_display_matches(search_string, maximum_results_per_column, connection_string);
-        cout << clear_eol << "\n";
+        fmt::print("{}\n", clear_eol);
         return 0;
     }
     catch (odbc_soci_error & e)
